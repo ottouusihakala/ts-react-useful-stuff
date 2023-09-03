23dccd272764557d7822a78d81109b9264e0475e
Otto Uusihakala <otto.uusihakala@gmail.com>
2023-09-03 16:37:11 +0300

    Added remove methods

--- src/DiGraph.ts
+++ src/DiGraph.ts
@@ -11,7 +11,32 @@ class DiGraph {
     this.vertices.push(originVertex)
     this.vertices.push(destinationVertex)
     const adjacentsForOrigin = this.adjacents.get(originVertex) || []
-    this.adjacents.set(originVertex, [...adjacentsForOrigin, destinationVertex])
+    if (!adjacentsForOrigin.includes(destinationVertex)) {
+      adjacentsForOrigin.push(destinationVertex)
+    }
+    this.adjacents.set(originVertex, adjacentsForOrigin)
+  }
+
+  addVertex(vertex: Number) {
+    this.vertices.push(vertex)
+  }
+
+  removeEdge(origin: Number, destination: Number) {
+    const adjacentsForOrigin = this.adjacents.get(origin)
+    if (adjacentsForOrigin) {
+      this.adjacents.set(origin, adjacentsForOrigin.filter((v) => v !== destination))
+    }
+  }
+
+  removeVertex(vertex: Number) {
+    const vertexIndex = this.vertices.findIndex((v) => v === vertex)
+    if (vertexIndex > -1) {
+      this.vertices.splice(vertexIndex)
+      this.adjacents.delete(vertex)
+      this.adjacents.forEach((_, currentVertex) => {
+        this.removeEdge(currentVertex, vertex)
+      })
+    }
   }
 }
 
--- src/__tests__/DiGraph.test.ts
+++ src/__tests__/DiGraph.test.ts
@@ -8,17 +8,78 @@ describe('DiGraph', () => {
       expect(graph.vertices).toContain(1)
       expect(graph.vertices).toContain(2)
     })
+
     it('Adds edge from origin to destination vertex', () => {
       const graph = new DiGraph()
       graph.addEdge(1, 2)
       expect(graph.adjacents.get(1)).toContain(2)
     })
+
     it('Does not add edge from destination to vertex', () => {
       const graph = new DiGraph()
       graph.addEdge(1, 2)
       graph.addEdge(2, 3)
       expect(graph.adjacents.get(2)).not.toContain(1)
       expect(graph.adjacents.get(3)).toBeFalsy()
+    })
+
+    it('Does not add duplicate destination vertices', () => {
+      const graph = new DiGraph()
+      graph.addEdge(1, 2)
+      graph.addEdge(1, 2)
+      expect(graph.adjacents.get(1)?.length).toBe(1)
+      expect(graph.adjacents.get(1)).toContain(2)
+    })
+  })
+
+  describe('removeEdge', () => {
+    it('Removes edge from origin to destination', () => {
+      const graph = new DiGraph()
+      graph.addEdge(1, 2)
+      graph.removeEdge(1, 2)
+      expect(graph.adjacents.get(1)).not.toContain(2)
+    })
+
+    it('Does not remove origin nor destination', () => {
+      const graph = new DiGraph()
+      graph.addEdge(1, 2)
+      graph.removeEdge(1, 2)
+      expect(graph.vertices).toContain(1)
+      expect(graph.vertices).toContain(2)
+    })
+
+    it('Does not remove connection from destination to origin', () => {
+      const graph = new DiGraph()
+      graph.addEdge(1, 2)
+      graph.addEdge(2, 1)
+      graph.removeEdge(1, 2)
+      expect(graph.adjacents.get(1)).not.toContain(2)
+      expect(graph.adjacents.get(2)).toContain(1)
+    })
+  })
+
+  describe('removeVertex', () => {
+    it('Removes vertex', () => {
+      const graph = new DiGraph()
+      graph.addEdge(1, 2)
+      graph.removeVertex(2)
+      expect(graph.vertices).not.toContain(2)
+    })
+    it('Removes all edges from the vertex', () => {
+      const graph = new DiGraph()
+      graph.addEdge(1, 2)
+      graph.addEdge(1, 3)
+      graph.removeVertex(1)
+      expect(graph.adjacents.get(1)).toBeFalsy()
+    })
+
+    it('Removes all edges to the vertex', () => {
+      const graph = new DiGraph()
+      graph.addEdge(1, 2)
+      graph.addEdge(3, 2)
+      graph.removeVertex(2)
+      expect(graph.adjacents.get(1)).not.toContain(2)
+      expect(graph.adjacents.get(3)).not.toContain(2)
     })
   })
 })
